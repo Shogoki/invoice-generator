18cb930da9017422e11158f53ef5d6d83490807a
Sven Igl <shogoki@gamersvillage.de>
2020-03-27 22:00:02 +0100

    remove unneccessary file

--- backend/backend/resolve_batcher.py
+++
@@ -1,9 +0,0 @@
-from ariadne.types import ExtensionSync
-
-
-class ResolveBatcher(ExtensionSync):
-
-    def resolve(self, next_, parent, info, **kwargs):
-        print("RESOLVE HOOK CALLED:\nnext: {}\nparent: {}\nInfo:{}".format(
-            next_, parent, info))
-        return next_(parent, info)
